7c2c8174c9ce6457e74491a377656e975c436f8a
Luis Eduardo González Vargas <luedgova1967@hotmail.com>
2019-03-07 21:46:12 -0500

    LoginPage and InstanceLocator

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModels Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModels();
+        }
+        #endregion
+    }
+}
